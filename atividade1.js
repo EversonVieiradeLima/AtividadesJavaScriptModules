8859f21f680e99c786e9fe87d41621feab2af70b
Éverson Vieira de Lima <eversonvieiradelima@gmail.com>
2023-06-26 17:28:10 -0300

    Atividade do dia 26/06/2023 - Tquinta
    Criar 14 variáveis, 2 de cada tipo de variável

---
+++ atividade1.js
@@ -0,0 +1,32 @@
+/*
+Crie 14 variaveis, 2 de cada tipo:
+Number, String, Boolean, Array, Object, Undefined e Null
+*/
+
+var porcao = 9.5;
+var qunatidade = 10;
+console.log("Variáveis numeriacas:", porcao, "e", qunatidade);
+
+var nome = "Éverson";
+var cidade = "São Lourenço da Mata";
+console.log("Variáveis do tipo string:", nome, "e", cidade);
+
+var eversonSlm = true;
+var eversonRec = false;
+console.log("Variáveis do tipo Boolean:", eversonSlm, "e", eversonRec);
+
+var cidades = ["São Lourenço da Mata", "Recife", "Camaragibe"];
+var bairros = ["Caxangá", "Cordeiro", "Vila do Reinado"];
+console.log("Variáveis do tipo Array:", cidades, "e", bairros);
+
+var aluno = { nome: "Éverson", idade: 24, turmaFront: true, turmaBack: false };
+var cliente = { nome: "Cláudio", cpf: "123.456.789-00", cel: 81912345678 };
+console.log("Variáveis do tipo Object:", aluno, "e", cliente);
+
+var nomeCompleto;
+var idade;
+console.log("Variáveis do tipo Undefined:", nomeCompleto, "e", idade);
+
+var localizacao = null;
+var identificacao = null;
+console.log("Variáveis do tipo Null:", localizacao, "e", identificacao);
