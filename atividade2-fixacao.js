98545635ecb828d4d0b5907efa18962a6a41f4f8
Éverson Vieira de Lima <eversonvieiradelima@gmail.com>
2023-06-26 17:41:11 -0300

    Atividade de fixação do dia 26/06/2023 - Tquinta
    Criação de 2 variáveis utilizando as palavras-chave var, let e const.

---
+++ atividade2-fixacao.js
@@ -0,0 +1,16 @@
+/*
+1) Criar duas variáveis usando a palavra-chave var.
+
+2) Criar duas variáveis usando a palavra-chave let.
+
+3) Criar duas constantes usando a palavra-chave const.
+*/
+
+var idade = 24;
+var cidade = "São Lourenço da Mata";
+
+let cor = "Azul";
+let verdadeiro = true;
+
+const nome = "Éverson";
+const cpf = "123.456.789-00";
